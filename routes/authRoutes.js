d601ffcdeebfb1d2223cdfe6240eb0d21a594b5e
Ben Hong <bencodezen@gmail.com>
2017-07-30 17:04:55 -0400

    feat: Add ability to add session cookies and pull out current user

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,4 +12,8 @@ module.exports = app => {
 	// This is the redirect-uri where Google returns to when it is successful
 	// at authenticated
 	app.get('/auth/google/callback', passport.authenticate('google'))
+
+	app.get('/api/current_user', (req, res) => {
+		res.send(req.user)
+	})
 }
