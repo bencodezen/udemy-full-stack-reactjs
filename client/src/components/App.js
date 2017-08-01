8b5666b434c3283672bbafdfd2ec413e37c9ef21
Ben Hong <bencodezen@gmail.com>
2017-07-31 21:54:09 -0400

    feat: Add basic routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+const Dashboard = () => <h2>Dashboard</h2>
+const Survey = () => <h2>Survey</h2>
+const Landing = () => <h2>Landing</h2>
 
 const App = () => {
-	return <h1>Hi there!</h1>
+	return (
+		<div>
+			<BrowserRouter>
+				<div>
+					<Route path="/" component={Landing} exact={true} />
+					<Route path="/dashboard" component={Dashboard} />
+					<Route path="/survey" component={Survey} />
+				</div>
+			</BrowserRouter>
+		</div>
+	)
 }
 
 export default App
