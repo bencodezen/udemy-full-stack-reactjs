bddfb5e54b969c98afd22575e1f4d49ae2356a56
Ben Hong <bencodezen@gmail.com>
2017-07-31 22:23:38 -0400

    feat: Wire up fetchUser on App didMount event

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,22 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import * as actions from '../actions'
 
 const Dashboard = () => <h2>Dashboard</h2>
 const Survey = () => <h2>Survey</h2>
 const Landing = () => <h2>Landing</h2>
 
-const App = () => {
-	return (
-		<div>
-			<BrowserRouter>
-				<div>
-					<Route path="/" component={Landing} exact={true} />
-					<Route path="/dashboard" component={Dashboard} />
-					<Route path="/survey" component={Survey} />
-				</div>
-			</BrowserRouter>
-		</div>
-	)
+class App extends Component {
+	componentDidMount() {
+		this.props.fetchUser()
+	}
+
+	render() {
+		return (
+			<div>
+				<BrowserRouter>
+					<div>
+						<Route path="/" component={Landing} exact={true} />
+						<Route path="/dashboard" component={Dashboard} />
+						<Route path="/survey" component={Survey} />
+					</div>
+				</BrowserRouter>
+			</div>
+		)
+	}
 }
 
-export default App
+export default connect(null, actions)(App)
